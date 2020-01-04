be51ce363cbb610e84d3362c65510b143ec85523
millerpractice <sarahla@abv.bg>
2020-01-04 09:20:18 +0100

    text correction

--- src/Team.js
+++ src/Team.js
@@ -20,12 +20,12 @@ export default class Team extends Component {
                             <img src={arzt} alt="arzt" style={{ float: "left", height: 240, marginTop: 70, paddingLeft: 25,
                                 marginRight: -100, marginLeft: -70 }} />
                             <Typography style={{ color: "white", paddingTop: 70, fontSize: 17, fontWeight: 500, textAlign: "left", paddingLeft: 230, paddingRight: 80 }}>
-                                Dr.Moris Miller studied medicine in the university in Frankfurt am Main.
+                                Dr. Moris Miller studied medicine at the university in Frankfurt am Main.
     </Typography>
                             <Typography style={{ color: "white", fontSize: 17, fontWeight: 500, textAlign: "left", paddingLeft: 270, paddingTop: 20, paddingRight: 80 }}>
                                 After his graduation in 1998,
                                 he gained experience in different clinics and medical centers and spent a couple of years in England where he gathered
-                                expert knowledge in the fields of surgical diagnostic and therapy, cardiology, radiology and rehabilitation. At that time Dr.Miller
+                                expert knowledge in the fields of surgical diagnostic and therapy, cardiology, radiology and rehabilitation. At that time Dr. Miller
                                 was announced Member of the Royal College of General Practitioners in London.
     </Typography>
                             <Typography style={{ color: "white", fontSize: 17, fontWeight: 500, textAlign: "left", marginLeft: 272, paddingTop: 20, paddingRight: 80 }}>
@@ -36,7 +36,7 @@ export default class Team extends Component {
                         <Grid item xs={6}>
                             <img src={kaira} alt="kaira" style={{ float: "left", height: 280, marginTop: 40, marginRight:-70, marginLeft:-70 }} />
                             <Typography style={{ color: "white", paddingTop: 70, fontSize: 17, fontWeight: 500, textAlign: "left", paddingLeft: 260, paddingRight: 100 }}>
-                                Kaira is working as Dr.Miller’s qualified medical assistant since 2018.
+                                Kaira is working as Dr. Miller’s qualified medical assistant since 2018.
 </Typography>
                             <Typography style={{ color: "white", paddingTop: 20, fontSize: 17, fontWeight: 500, textAlign: "left", paddingLeft: 260, paddingRight: 100 }}>
                                 She was developed from researchers from Showa University
@@ -45,7 +45,7 @@ export default class Team extends Component {
                                 such as determining if a lesion is cancerous and make a lifetime decision.
 </Typography>
                             <Typography style={{ color: "white", fontSize: 17, fontWeight: 500, textAlign: "left", marginLeft: 210, paddingTop: 20, paddingLeft: 50, paddingRight: 100 }}>
-                                Besides cancer  Kaira is currently supporting Dr.Miller to diagnose diseases such as asthma, diabetes and skin disease.
+                                Besides cancer prediction Kaira is currently supporting Dr. Miller to diagnose diseases such as asthma, diabetes and skin disease.
     </Typography>
                         </Grid>
                     </Grid>
